74a614849b6dbc45626836db4f64dadf5300025c
yewon <yewon5101951@naver.com>
2026-07-17 11:40:10 +0900

    fifth commit

--- script.js
+++ script.js
@@ -2,8 +2,31 @@
 let batteryLevel=100;
 
 //alarm setting
-// const setAlarmListElement = document.getElementById("alarmSettingAddBtn");
-// setAlarmListElement.addEventListener("click", setAlarm);
+const maxAlarmListLength=3;
+let alarmListElementCurrentNumber=0;
+
+const setAlarmListElement=document.getElementById("alarmSettingAddBtn");
+const setInitAlarmListElement=document.getElementById("initAlarmListElementBtn")
+
+setAlarmListElement.addEventListener("click", setAlarm);
+setInitAlarmListElement.addEventListener("click", setInitAlarm);
+
+//도메인 설정
+// document.addEventListener("DOMContentLoaded", ()=>{
+//     const numberInput=document.querySelectorAll('input[type="number"]')
+
+//     numberInputs.forEach(input=>{
+//         input.addEventListener("keydown", (event)=>{
+//             if(['.','-','e','E'].includes(event.key)){
+//                 event.preventDefault();
+//             }
+//         })
+
+//         numberInputs.addEventListener("input", function(){
+//             this.value=this.value.replace(/[^0-9]/g, '');
+//         });
+//     })
+// });
 
 function getTime(){
     const currentDateTime=new Date(); //현재 날짜와 시간 객체 생성
@@ -35,20 +58,36 @@ function displayHeadTime(){
 function updateBattery(){
     document.getElementById('batteryLevel').innerText=`배터리잔량:${batteryLevel}%`;
     
-    if (batteryLevel>0) {
+    if(batteryLevel>0){
         batteryLevel=batteryLevel-1;
     }
 }
 
-// function setAlarm(){
-//     const SettedAlarmHour=parseInt(document.getElementById('alarmSettingHour'));
-//     const SettedAlarmMinutes=parseInt(document.getElementById('alarmSettingMinutes'));
-//     const SettedAlarmSeconds=parseInt(document.getElementById('alarmSettingSeconds'));
+function setAlarm(){
+    if(alarmListElementCurrentNumber>maxAlarmListLength){
+        alert("알람은 최대 3개까지만 설정가능합니다. 삭제 후 재시도 해주십시오.")
+        return;
+    }
+    alarmListElementCurrentNumber+=1;
 
-//     function makeSettedAlarmList{
-//         const 
-//     }
-// }
+    const hourInput=document.getElementById('alarmSettingHour');
+    const minInput=document.getElementById('alarmSettingMinutes');
+    const secInput=document.getElementById('alarmSettingSeconds');
+
+    const settedAlarmHour=String(hourInput ? hourInput.value : "0" || "0").padStart(2, '0');
+    const settedAlarmMinutes=String(minInput ? minInput.value : "0" || "0").padStart(2, '0');
+    const settedAlarmSeconds=String(secInput ? secInput.value : "0" || "0").padStart(2, '0');
+
+    document.getElementById(`alarmSettingElement${alarmListElementCurrentNumber}`).innerHTML=`${settedAlarmHour}:${settedAlarmMinutes}:${settedAlarmSeconds}`;
+}
+
+function setInitAlarm(){
+    for(let i=1; i<=maxAlarmListLength;i++){
+        document.getElementById(`alarmSettingElement${i}`).innerHTML=`00:00:00`;
+    }
+
+    alarmListElementCurrentNumber=0;
+}
 
 displayHeadTime();
 setInterval(displayHeadTime, 1000);
